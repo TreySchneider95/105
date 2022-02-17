f84aa78589b3ad009c6df0f5fd090f4b55314117
TreySchneider95 <tschneid95@gmail.com>
2022-02-17 10:35:21 -0700

    day 3

--- users_day2/static/script/storeManager.js
+++ users_day2/static/script/storeManager.js
@@ -1,8 +1,17 @@
+const KEY_VALUE = "users"
 function saveUser(user){
-    let val = JSON.stringify(user)
-    localStorage.setItem('users', val)
+    let oldData = readUser()
+    oldData.push(user)
+    let val = JSON.stringify(oldData)
+    localStorage.setItem(KEY_VALUE, val)
 
 }
 function readUser(){
-
+    let users = localStorage.getItem(KEY_VALUE)
+    if(!users){
+        return []
+    }else{
+        let list = JSON.parse(users)
+        return list
+    }
 }
